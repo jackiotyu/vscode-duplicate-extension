56ddb736679386c23ee62439cc9aef13e4d68c03
jackhuang <2504448153@qq.com>
2024-04-11 17:57:44 +0800

    feat: 添加中文、删除文件夹命令
    Signed-off-by: jackhuang <2504448153@qq.com>

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,11 @@ export enum Commands {
     refresh = 'duplicate-extension.refresh',
     filter = 'duplicate-extension.filter',
     unFilter = 'duplicate-extension.unFilter',
-    revealInExtensions = 'duplicate-extension.revealInExtensions'
+    revealInExtensions = 'duplicate-extension.revealInExtensions',
+    cleanupOutdatedExtensions = 'duplicate-extension.cleanupOutdatedExtensions',
+    moveExtensionToTrashcan = 'duplicate-extension.moveExtensionToTrashcan',
+}
+
+export enum ContextValue {
+    filter = 'duplicate-extension.filter',
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
 import os from 'os';
 import path from 'path';
-import fs from 'fs/promises';
 import semver from 'semver';
 import debounce from 'lodash/debounce';
 import { ExtInfo } from '@/types';
-import { Commands } from '@/constants';
+import { Commands, ContextValue } from '@/constants';
 
 const extFolderUri = vscode.env.appName.includes('Code - Insiders')
     ? vscode.Uri.file(path.join(os.homedir(), '.vscode-insiders', 'extensions'))
@@ -19,9 +18,10 @@ const findAllPackageJSON = async (): Promise<ExtInfo[]> => {
     const jsonInfoList = await Promise.all(
         res.map(async (file) => {
             try {
-                const mtime = (await fs.stat(file.fsPath)).mtime.getTime();
-                const text = await fs.readFile(file.fsPath, 'utf-8');
-                const { name, displayName, description, version, publisher } = JSON.parse(text);
+                const fileUri = vscode.Uri.file(file.fsPath);
+                const mtime = (await vscode.workspace.fs.stat(fileUri)).mtime;
+                const text = await vscode.workspace.fs.readFile(fileUri);
+                const { name, displayName, description, version, publisher } = JSON.parse(Buffer.from(text).toString());
                 return { name, displayName, description, version, publisher, mtime, path: path.dirname(file.fsPath) };
             } catch {
                 return null;
@@ -34,16 +34,26 @@ const findAllPackageJSON = async (): Promise<ExtInfo[]> => {
     });
 };
 
-class ExtTreeItem extends vscode.TreeItem {
-    duplicate: boolean = false;
-    contextValue = 'duplicate-extension.extInfo';
-    extId: string = '';
-}
+const removeFolders = async (items: { path: string }[], message: string, detail = '') => {
+    try {
+        const confirm = 'confirm';
+        const cancel = 'cancel';
+        const res = await vscode.window.showInformationMessage(message, { detail, modal: true }, confirm, cancel);
+        if (res !== confirm) return;
+        await Promise.all(
+            items.map((item) =>
+                vscode.workspace.fs.delete(vscode.Uri.file(item.path), { recursive: true, useTrash: true }),
+            ),
+        );
+        vscode.window.showInformationMessage("Move to system's trashcan succeeded");
+    } catch (error: any) {
+        vscode.window.showErrorMessage(error.message);
+    }
+};
 
-class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-    static readonly id = 'duplicate-extension.extension-version-list';
-    private data: ExtInfo[] = [];
-    private readonly colorList = [
+class ColorSelector {
+    private static duplicateName: string = '';
+    static readonly colorList = [
         'terminal.ansiBlue',
         'terminal.ansiGreen',
         'terminal.ansiMagenta',
@@ -51,6 +61,64 @@ class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
         'terminal.ansiWhite',
         'terminal.ansiYellow',
     ];
+    private static colorIndex: number = 0;
+    static getColor(name: string): vscode.ThemeColor | undefined {
+        if (name === this.duplicateName || this.duplicateName === '') {
+            const color = this.colorList[this.colorIndex];
+            this.duplicateName = name;
+            return new vscode.ThemeColor(color);
+        } else {
+            if (this.colorIndex === this.colorList.length - 1) this.colorIndex = 0;
+            else this.colorIndex++;
+            this.duplicateName = name;
+            const color = this.colorList[this.colorIndex];
+            return new vscode.ThemeColor(color);
+        }
+    }
+    static reset() {
+        this.colorIndex = 0;
+        this.duplicateName = '';
+    }
+}
+
+class ExtTreeItem extends vscode.TreeItem {
+    duplicate: boolean = false;
+    contextValue = 'duplicate-extension.extInfo';
+    extId: string = '';
+    path: string = '';
+    constructor(item: ExtInfo, isDuplicate: boolean, collapsibleState?: vscode.TreeItemCollapsibleState) {
+        const extId = item.publisher + '.' + item.name;
+        const ext = vscode.extensions.getExtension(extId);
+        const isActiveExt = ext?.extensionPath === item.path;
+        super(isActiveExt ? `${item.name} ✨` : item.name, collapsibleState);
+        this.description = item.version;
+
+        this.tooltip = new vscode.MarkdownString('');
+        this.tooltip.appendMarkdown(`### ${item.name}\n`);
+        this.tooltip.appendMarkdown(`- displayName: *${ext?.packageJSON.description || item.displayName}*\n`);
+        this.tooltip.appendMarkdown(`- description: *${ext?.packageJSON.description || item.description}*\n`);
+        this.tooltip.appendMarkdown(`- version: ${item.version}\n`);
+        this.tooltip.appendMarkdown(`- publisher: *${item.publisher}*\n`);
+        this.tooltip.appendMarkdown(`- mtime: ${new Date(item.mtime).toLocaleString()}\n`);
+        this.tooltip.appendMarkdown(`- path: *${item.path}*\n`);
+
+        this.iconPath = isDuplicate
+            ? new vscode.ThemeIcon('check-all', ColorSelector.getColor(item.name))
+            : new vscode.ThemeIcon('blank');
+        this.duplicate = isDuplicate;
+        this.command = {
+            command: 'revealFileInOS',
+            title: 'open',
+            arguments: [vscode.Uri.file(item.path)],
+        };
+        this.extId = extId;
+        this.path = item.path;
+    }
+}
+
+class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+    static readonly id = 'duplicate-extension.extension-version-list';
+    private data: ExtInfo[] = [];
     private useFilter = false;
     _onDidChangeTreeData = new vscode.EventEmitter<void>();
     onDidChangeTreeData = this._onDidChangeTreeData.event;
@@ -58,6 +126,8 @@ class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
         const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(extFolderUri, '*'));
         context.subscriptions.push(
             watcher.onDidChange(() => this.refresh()),
+            watcher.onDidDelete(() => this.refresh()),
+            watcher.onDidCreate(() => this.refresh()),
             watcher,
             refreshEvent.event(this.refresh),
             filterEvent.event(this.toggleFilter),
@@ -69,8 +139,7 @@ class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
         this.refresh();
     };
     refresh = debounce(async () => {
-        this.colorIndex = 0;
-        this.duplicateName = '';
+        ColorSelector.reset();
         this.data = await findAllPackageJSON();
         this._onDidChangeTreeData.fire();
     }, 300);
@@ -86,58 +155,32 @@ class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
         });
         return set;
     }
-    private colorIndex: number = 0;
-    private duplicateName: string = '';
-    private getColor(name: string): vscode.ThemeColor | undefined {
-        if (name === this.duplicateName || this.duplicateName === '') {
-            const color = this.colorList[this.colorIndex];
-            this.duplicateName = name;
-            return new vscode.ThemeColor(color);
-        } else {
-            if (this.colorIndex === this.colorList.length - 1) this.colorIndex = 0;
-            else this.colorIndex++;
-            this.duplicateName = name;
-            const color = this.colorList[this.colorIndex];
-            return new vscode.ThemeColor(color);
-        }
+    getOutdatedExtInfos() {
+        let map = new Map<string, ExtInfo[]>();
+        this.data.forEach((item) => {
+            if (map.has(item.name)) {
+                map.get(item.name)!.push(item);
+            } else {
+                map.set(item.name, [item]);
+            }
+        });
+        let list: ExtInfo[] = [];
+        map.forEach((value) => {
+            if (value.length <= 1) return;
+            const exts = value.sort((a, b) => (semver.gt(a.version, b.version) ? 1 : -1));
+            exts.pop();
+            list.push(...exts);
+        });
+        return list;
     }
     getChildren(element?: vscode.TreeItem | undefined): vscode.ProviderResult<vscode.TreeItem[]> {
         if (!element) {
             const duplicateSet = this.findDuplicateSet();
             const items = this.data.map((item) => {
                 const isDuplicate = duplicateSet.has(item.name);
-                const extId = item.publisher + '.' + item.name;
-                const ext = vscode.extensions.getExtension(extId);
-                const isActiveExt = ext?.extensionPath === item.path;
-                const treeItem = new ExtTreeItem(isActiveExt ? `${item.name} ✨` : item.name);
-                treeItem.description = item.version;
-
-                treeItem.tooltip = new vscode.MarkdownString('');
-                treeItem.tooltip.appendMarkdown(`### ${item.name}\n`);
-                treeItem.tooltip.appendMarkdown(
-                    `- displayName: *${ext?.packageJSON.description || item.displayName}*\n`,
-                );
-                treeItem.tooltip.appendMarkdown(
-                    `- description: *${ext?.packageJSON.description || item.description}*\n`,
-                );
-                treeItem.tooltip.appendMarkdown(`- version: ${item.version}\n`);
-                treeItem.tooltip.appendMarkdown(`- publisher: *${item.publisher}*\n`);
-                treeItem.tooltip.appendMarkdown(`- mtime: ${new Date(item.mtime).toLocaleString()}\n`);
-                treeItem.tooltip.appendMarkdown(`- path: *${item.path}*\n`);
-
-                treeItem.iconPath = isDuplicate
-                    ? new vscode.ThemeIcon('check-all', this.getColor(item.name))
-                    : new vscode.ThemeIcon('blank');
-                treeItem.duplicate = isDuplicate;
-                treeItem.command = {
-                    command: 'revealFileInOS',
-                    title: 'open',
-                    arguments: [vscode.Uri.file(item.path)],
-                };
-                treeItem.extId = extId;
-                return treeItem;
+                return new ExtTreeItem(item, isDuplicate);
             });
-            if(this.useFilter) return items.filter(i => i.duplicate);
+            if (this.useFilter) return items.filter((i) => i.duplicate);
             return items;
         }
         return [];
@@ -148,22 +191,35 @@ class TreeViewProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    vscode.commands.executeCommand('setContext', 'duplicate-extension.filter', false);
+    vscode.commands.executeCommand('setContext', ContextValue.filter, false);
+    const treeDataProvider = new TreeViewProvider(context);
     context.subscriptions.push(
-        vscode.window.registerTreeDataProvider(TreeViewProvider.id, new TreeViewProvider(context)),
+        vscode.window.registerTreeDataProvider(TreeViewProvider.id, treeDataProvider),
         vscode.commands.registerCommand(Commands.refresh, () => {
             refreshEvent.fire();
         }),
         vscode.commands.registerCommand(Commands.filter, () => {
-            vscode.commands.executeCommand('setContext', 'duplicate-extension.filter', true);
+            vscode.commands.executeCommand('setContext', ContextValue.filter, true);
             filterEvent.fire(true);
         }),
         vscode.commands.registerCommand(Commands.unFilter, () => {
-            vscode.commands.executeCommand('setContext', 'duplicate-extension.filter', false);
+            vscode.commands.executeCommand('setContext', ContextValue.filter, false);
             filterEvent.fire(false);
         }),
         vscode.commands.registerCommand(Commands.revealInExtensions, (item: ExtTreeItem) => {
             vscode.commands.executeCommand('workbench.extensions.action.showExtensionsWithIds', [[item.extId]]);
+        }),
+        vscode.commands.registerCommand(Commands.moveExtensionToTrashcan, (item: ExtTreeItem) => {
+            removeFolders([item], vscode.l10n.t("Move extension folder to system's trashcan?"), vscode.l10n.t("Selected folder is {0}", item.path));
+        }),
+        vscode.commands.registerCommand(Commands.cleanupOutdatedExtensions, () => {
+            const outdatedExtInfos = treeDataProvider.getOutdatedExtInfos();
+            if (!outdatedExtInfos.length) return;
+            removeFolders(
+                outdatedExtInfos,
+                vscode.l10n.t("Move outdated extensions to system's trashcan?"),
+                vscode.l10n.t("Selected folders:\n{0}", outdatedExtInfos.map(item => item.path).join('\n'))
+            );
         }),
         refreshEvent,
         filterEvent,
